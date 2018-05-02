eecedb718efc93ae72d0d641507ae2532c323f5d
zzh2616 <zzh2616@hotmail.com>
2018-05-03 00:15:22 +0200

    problem 2 solved

--- hog.py
+++ hog.py
@@ -46,6 +46,13 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     assert opponent_score < 100, 'The game should be over.'
     "*** YOUR CODE HERE ***"
+    score = 0
+    if num_rolls != 0:
+        score = roll_dice(num_rolls, dice)
+    else:
+        # free bacon rule
+        score = max(int(opponent_score / 10), opponent_score % 10) + 1
+    return score
 
 # Playing a game
 
